b7d36ee58cc70b2b056d40a1acfa32febcf10e8c
AzureAD\MaximeBarrera <maxime.barrera@toulouse.miage.fr>
2024-11-15 16:13:05 +0100

    Maxime - VoyageImpl et Interface Voyage

--- src/main/java/org/example/Voyage.java
+++ src/main/java/org/example/Voyage.java
@@ -1,2 +1,9 @@
-package org.example;public interface Voyage {
+package org.example;
+
+public interface Voyage {
+
+    /*
+     * Afficher les logements d'une personne
+     */
+    public void afficherMesVoyages(utilisateur u);
 }
